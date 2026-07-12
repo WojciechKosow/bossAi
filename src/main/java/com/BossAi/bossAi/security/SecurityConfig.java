4393b973f31528b6c7ad941a01dcc96fce5f8225
wojte <wojciech@kosow.pl>
2026-07-12 22:55:31 +0200

    test: testing

--- src/main/java/com/BossAi/bossAi/security/SecurityConfig.java
+++ src/main/java/com/BossAi/bossAi/security/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
+                                "/api/test",
                                 "/api/auth/**",
                                 "/oauth2/**",
                                 "/internal/**",
